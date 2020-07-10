ac901ecfe27a3af99b29d1814ed8374e338e2fca
Shekhar <patnaikshekhar@gmail.com>
2020-07-10 16:49:59 +0000

    Generic experiementation

--- src/kube_client.rs
+++ src/kube_client.rs
@@ -1,11 +1,16 @@
 use crate::inode::K8sInteractions;
-use k8s_openapi::api::core::v1::{Pod, Namespace, Service};
-use k8s_openapi::api::apps::v1::{StatefulSet, Deployment};
+use k8s_openapi::api::apps::v1::{Deployment, StatefulSet};
+use k8s_openapi::api::core::v1::{Namespace, Pod, Service};
+
+use k8s_openapi::Resource;
+use serde::de::DeserializeOwned;
+use std::clone::Clone;
 
 use kube::{
     api::{ListParams, Meta},
     Api, Client,
 };
+
 use tokio::runtime::Runtime;
 
 pub struct KubeClient {
@@ -23,13 +28,31 @@ impl KubeClient {
         }
     }
 
-    async fn get_objects(&mut self, namespace: &str, x: T) -> Result<Vec<String> {
-        let objects: Api<T> = Api::namespaced(self.client.clone(), namespace);
+    // async fn get_objects(&mut self, namespace: &str, objects: Api<Resource>) -> anyhow::Result<Vec<String>> {
+    //     // let objects: Api<T> = Api::namespaced(self.client.clone(), namespace);
+
+    //     let lp = ListParams::default();
+
+    //     let objectList = objects.list(lp).await?;
+
+    //     Ok(objectList.iter().map(|o| { Meta::name(o) }))
+    // }
+}
+
+struct KubeObject<T: Resource + Clone + DeserializeOwned + Meta> {
+    x: T,
+    client: Client,
+}
+
+impl<T: Resource + Clone + DeserializeOwned + Meta> KubeObject<T> {
+    async fn get_objects(&self, namespace: &str) -> Result<Vec<String>, anyhow::Error> {
+        let objects: Api<T> = Api::<T>::namespaced(self.client.clone(), namespace);
+
         let lp = ListParams::default();
 
-        let objectList = objects.list(lp).await?;
+        let objectList = objects.list(&lp).await?;
 
-        Ok(objectList.iter().map(|o| { Meta::name(o) }))
+        Ok(objectList.iter().map(|o| Meta::name(o)).collect())
     }
 }
 
@@ -54,6 +77,21 @@ impl K8sInteractions for KubeClient {
         namespace: &str,
         object_name: &str,
     ) -> Result<Vec<String>, anyhow::Error> {
-        unimplemented!()
+        let lp = ListParams::default();
+        // objectList.iter().map(|o| { Meta::name(o) })
+
+        match object_name {
+            "deployments" => {
+                let objects = Api::<Deployment>::namespaced(self.client.clone(), namespace);
+                let objectList = self.runtime.block_on(objects.list(&lp))?;
+            }
+            "services" => {
+                let objects = Api::<Service>::namespaced(self.client.clone(), namespace);
+                let objectList = self.runtime.block_on(objects.list(&lp))?;
+            }
+            _ => {}
+        };
+
+        Ok(vec![])
     }
 }
